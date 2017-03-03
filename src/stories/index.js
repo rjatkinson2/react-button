a9840a769d015166eacbae439325871b8cc09079
Ryan Atkinson <rjatkinson2@gmail.com>
2017-03-02 17:31:58 -0800

    Remove unnecessary import from react-button example
    `import { action } from '@kadira/storybook'` created an unused import.

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { storiesOf, linkTo } from '@kadira/storybook';
 import Button from '../index';
 
 storiesOf('Button', module)
